67f86cd8990d6f1f4f9bf02ea690e8eb0cabbc34
ApurboShib <shibjoy088@gmail.com>
2026-01-30 23:49:03 +0600

    add authentication layout and pages; include react-hook-form dependency

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,24 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/home/Home";
 import RootLayouts from "../layouts/RootLayouts";
+import AuthLayout from "../AuthLayout/AuthLayout";
+import { Component } from "react";
+import Login from "../pages/Authentication/Login/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayouts,
     children: [{ index: true, Component: Home }],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
   },
 ]);
