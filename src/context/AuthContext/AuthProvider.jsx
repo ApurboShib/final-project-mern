6111d38e04d42428c634d4a6b5262683debb1a50
ApurboShib <shibjoy088@gmail.com>
2026-01-31 16:03:36 +0600

    feat: implement authentication functions and user state management in AuthProvider

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,15 +1,58 @@
-import AuthContext from './AuthCOntext';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
+import { useEffect, useState } from "react";
+
 
 const AuthProvider = ({ children }) => {
+  // check that is that user is created or not?
 
-    const authInfo = {
+  const [user, setUser] = useState(null);
 
-    }
-    return (
-        <AuthContext value={authInfo}>
-            {children}
-        </AuthContext>
-    );
+  // loading
+
+  const [loading, setLoading] = useState(true);
+  // create user function
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // create sign in info
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // loged in user observer
+
+  const loggedOut = (auth) => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    // callback functions
+
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    loggedOut,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
